3730e40b28122fb007a8661e0b215778a40ec46b
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2023-12-18 22:10:54 +0500

    feat: water for the main scene

--- Assets/Obsticle.cs
+++ Assets/Obsticle.cs
@@ -5,8 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class Obsticle : MonoBehaviour
 {
-    [SerializeField] GameObject obsticle ;
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Main Character")
